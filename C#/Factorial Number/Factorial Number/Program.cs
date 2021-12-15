2db1dba9fc07fd61b7cdd1f1d6db31e708fda1a7
AntonyAjith <94164200+AntonyAjith@users.noreply.github.com>
2021-12-15 16:10:23 +0530

    Added:
    New File.

---
+++ C#/Factorial Number/Factorial Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nNumber;
+            double dfactorial;
+
+            Console.Write("Enter a Number: ");
+            nNumber = Convert.ToInt32(Console.ReadLine());
+
+            dfactorial = Factorial(nNumber);
+            Console.WriteLine("factorial number of " + nNumber + " = " + dfactorial.ToString());
+
+        }
+
+        public static double Factorial(int nNumber)
+        {
+            if (nNumber == 0)
+                return 1;
+            return nNumber * Factorial(nNumber - 1);
+        }
+    }
+}
